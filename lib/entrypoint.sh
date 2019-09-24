a858f6c823b7ff941625b7178fd313ede4f284bc
Pat Ambrosio <1647347+hmngwy@users.noreply.github.com>
2019-09-24 15:53:39 +0800

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-npm install
+if [ -f yarn.lock ]; then
+    yarn --non-interactive --silent --ignore-scripts --production=false
+else
+    NODE_ENV=development npm install
+fi
 
 NODE_PATH=node_modules node /action/lib/run.js $1
